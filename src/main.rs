7842be1b1aedf3051f7cb675e312699f01575d0b
netbrum <netbrum@tutanota.com>
2023-08-19 19:46:37 +0000

    add draw screen function and finish up main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 mod emulator;
 
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+
 use emulator::{display, Emulator};
 use sdl2::event::Event;
 use std::{
     env,
     fs::File,
     io::{self, Read},
-    process, thread,
-    time::Duration,
+    process,
 };
+
+const TICKS_PER_FRAME: usize = 10;
 
 // 64x32 is __really__ small on a modern screen so we scale it up
 const WINDOW_SCALE: u32 = 15;
@@ -18,8 +24,8 @@ const WINDOW_HEIGHT: u32 = display::DISPLAY_HEIGHT as u32 * WINDOW_SCALE;
 fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 2 {
-        println!("Missing path/to/rom");
+    if args.len() != 2 {
+        println!("Usage: path/to/rom");
         process::exit(1);
     }
 
@@ -34,7 +40,7 @@ fn main() -> Result<(), String> {
     let mut event_pump = sdl_context.event_pump()?;
     let video_subsystem = sdl_context.video()?;
     let window = video_subsystem
-        .window("august", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .window("chip8.rs", WINDOW_WIDTH, WINDOW_HEIGHT)
         .position_centered()
         .opengl()
         .build()
@@ -45,7 +51,7 @@ fn main() -> Result<(), String> {
         .present_vsync()
         .build()
         .expect("to build canvas");
-    canvas.clear();
+
     canvas.present();
 
     'main: loop {
@@ -58,10 +64,39 @@ fn main() -> Result<(), String> {
 
         emulator.keyboard.poll(&event_pump);
 
-        thread::sleep(Duration::from_millis(60));
+        for _ in 0..TICKS_PER_FRAME {
+            emulator.tick();
+        }
+
+        emulator.tick_timers();
+        draw_screen(&emulator, &mut canvas);
     }
 
     Ok(())
+}
+
+fn draw_screen(emulator: &Emulator, canvas: &mut Canvas<Window>) {
+    canvas.set_draw_color(Color::BLACK);
+    canvas.clear();
+
+    let frame_buffer = emulator.display.screen;
+
+    canvas.set_draw_color(Color::WHITE);
+
+    for (y, row) in frame_buffer.iter().enumerate() {
+        for (x, &col) in row.iter().enumerate() {
+            if col {
+                let x = (x as u32) * WINDOW_SCALE;
+                let y = (y as u32) * WINDOW_SCALE;
+
+                canvas
+                    .fill_rect(Rect::new(x as i32, y as i32, WINDOW_SCALE, WINDOW_SCALE))
+                    .expect("to draw rectangle on canvas");
+            }
+        }
+    }
+
+    canvas.present();
 }
 
 fn get_rom(args: Vec<String>) -> Result<Vec<u8>, io::Error> {
